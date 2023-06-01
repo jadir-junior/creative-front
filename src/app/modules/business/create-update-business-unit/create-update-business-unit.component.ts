5ca8c3962fccd6ea90ed4b000ded4898663106ee
Jadir Junior <jadirjsjunior@gmail.com>
2023-06-01 10:15:37 -0300

    refactor observable

--- src/app/modules/business/create-update-business-unit/create-update-business-unit.component.ts
+++ src/app/modules/business/create-update-business-unit/create-update-business-unit.component.ts
@@ -1,8 +1,9 @@
 import { BusinessService, BusinessUnit } from './../services/business.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MessageService } from '../../../components/toast/message.service';
-import { Column } from '../../../utils/models/pagination.model';
+import { Column, Pagination } from '../../../utils/models/pagination.model';
+import { Observable, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-create-update-business-unit',
@@ -42,49 +43,54 @@ import { Column } from '../../../utils/models/pagination.model';
         <p class="text-sm text-regular">Manage your business units</p>
       </div>
       <div class="ctv-business-container-table">
-        <div class="ctv-business-loading" *ngIf="loading">
-          <ctv-progress-spinner label="Loading..."></ctv-progress-spinner>
-        </div>
-        <ctv-not-data *ngIf="!value.length && !loading"></ctv-not-data>
-        <ctv-table
-          *ngIf="value.length && !loading"
-          [value]="value"
-          [columns]="cols"
-          variant="card"
-        >
-          <ng-template ctvTemplate="header" let-columns>
-            <tr>
-              <th *ngFor="let col of columns">{{ col.header }}</th>
-              <th>Actions</th>
-            </tr>
-          </ng-template>
-          <ng-template ctvTemplate="body" let-business let-columns="columns">
-            <tr>
-              <td *ngFor="let col of columns">{{ business[col.field] }}</td>
-              <td>
-                <ctv-button
-                  icon="delete"
-                  variant="text"
-                  color="secondary"
-                  [rounded]="true"
-                  (onClick)="deleteBusinessUnit(business.id)"
-                ></ctv-button>
-              </td>
-            </tr>
-          </ng-template>
-        </ctv-table>
+        <ng-container *ngIf="response$ | async as response; else loading">
+          <ctv-not-data *ngIf="!response.data.length"></ctv-not-data>
+          <ctv-table
+            *ngIf="response.data.length"
+            [value]="response.data"
+            [columns]="cols"
+            variant="card"
+          >
+            <ng-template ctvTemplate="header" let-columns>
+              <tr>
+                <th *ngFor="let col of columns">{{ col.header }}</th>
+                <th>Actions</th>
+              </tr>
+            </ng-template>
+            <ng-template ctvTemplate="body" let-business let-columns="columns">
+              <tr>
+                <td *ngFor="let col of columns">{{ business[col.field] }}</td>
+                <td>
+                  <ctv-button
+                    icon="delete"
+                    variant="text"
+                    color="secondary"
+                    [rounded]="true"
+                    (onClick)="deleteBusinessUnit(business.id)"
+                  ></ctv-button>
+                </td>
+              </tr>
+            </ng-template>
+          </ctv-table>
+        </ng-container>
+        <ng-template #loading>
+          <div class="ctv-business-loading">
+            <ctv-progress-spinner label="Loading..."></ctv-progress-spinner>
+          </div>
+        </ng-template>
       </div>
     </div>
   `,
   styleUrls: ['../create-update-business.css'],
 })
-export class CreateUpdateBusinessUnitComponent implements OnInit {
+export class CreateUpdateBusinessUnitComponent implements OnInit, OnDestroy {
+  destroyed: Subject<void> = new Subject<void>();
+
   form: FormGroup = this.fb.group({
     name: ['', [Validators.required]],
   });
 
-  value: BusinessUnit[] = [];
-  loading = false;
+  response$: Observable<Pagination<BusinessUnit[]>> | null = null;
 
   cols: Column[] = [
     {
@@ -104,33 +110,39 @@ export class CreateUpdateBusinessUnitComponent implements OnInit {
   }
 
   getBusinessUnits(): void {
-    this.loading = true;
-    this.businessService.listBusinessUnits().subscribe({
-      next: (response) => {
-        this.value = response.data;
-        this.loading = false;
-      },
-      error: () => (this.loading = false),
-    });
+    this.response$ = this.businessService.listBusinessUnits();
   }
 
-  cancel(): void {}
+  cancel(): void {
+    this.form.reset();
+  }
 
   onSubmit({ value }: FormGroup): void {
-    this.businessService.createBusinessUnit(value).subscribe(() => {
-      this.messageService.add({
-        severity: 'success',
-        summary: 'Successfully saved',
-        detail: 'The Business unit was saved successfully',
+    this.businessService
+      .createBusinessUnit(value)
+      .pipe(takeUntil(this.destroyed))
+      .subscribe(() => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successfully saved',
+          detail: 'The Business unit was saved successfully',
+        });
+        this.getBusinessUnits();
+        this.form.reset();
       });
-      this.getBusinessUnits();
-      this.form.reset();
-    });
   }
 
   deleteBusinessUnit(id: string): void {
-    this.businessService.deleteBusinessUnit(id).subscribe(() => {
-      this.getBusinessUnits();
-    });
+    this.businessService
+      .deleteBusinessUnit(id)
+      .pipe(takeUntil(this.destroyed))
+      .subscribe(() => {
+        this.getBusinessUnits();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
   }
 }
